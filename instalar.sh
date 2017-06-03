5be30428ad3f45053767c2ba8fb3f7c7040f9dd0
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-06-04 00:47:36 +0200

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 chmod 777 telnetd
 cp telnetd /sbin
+cp telnetd /etc/init.d/telnetd 
+update-rc.d telnetd start
+update-rc.d telnetd defaults
+service --status-all
+sudo /etc/init.d/telnetd restart
 telnetd
